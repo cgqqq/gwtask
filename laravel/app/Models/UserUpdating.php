585502a3b3ba1bee1a08963a70e4a430cf18d444
MaggiePengWS <1303010722@qq.com>
2018-04-04 20:48:40 +0800

    新增userUpdating用户个人动态数据表

---
+++ laravel/app/Models/UserUpdating.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Base;
+class UserUpdating extends Base
+{
+    //
+    protected $table = 'user_updating';
+
+
+}
---
+++ laravel/database/migrations/2018_04_04_124517_create_user_updating_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserUpdatingTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_updating', function (Blueprint $table) {
+            $table->string('id',32);
+            $table->string('updater_id',20);
+            $table->integer('time',false);
+            $table->string('content',10000);
+            $table->string('resource',100)->default(null)->nullable();
+            $table->enum('type',['cTeam','sResource','pUpdating']);
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_updating');
+    }
+}
